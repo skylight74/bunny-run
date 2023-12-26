c9f1b3d165f964bbb5d212a91f12e8493ea70c08
mohamed9974 <mohamed.amin@metu.edu.tr>
2023-12-26 21:55:38 +0300

    removed runners and configs

---
+++ include/ShaderProgram.h
@@ -0,0 +1,33 @@
+#ifndef SHADERPROGRAM_H
+#define SHADERPROGRAM_H
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+using namespace glm;
+
+class ShaderProgram {
+public:
+  ShaderProgram();
+  ~ShaderProgram();
+
+  void attachShader(const std::string &filename, GLenum shaderType);
+  void linkProgram();
+  void useProgram();
+
+  void setUniform(const string &name, float value);
+  void setUniform(const string &name, int value);
+  void setUniform(const string &name, const vec3 &value);
+  void setUniform(const string &name, const mat4 &value);
+
+private:
+  GLuint programID;
+  unordered_map<GLenum, GLuint> shaders;
+
+  GLuint loadShader(const string &filename, GLenum shaderType);
+};
+
+#endif // SHADERPROGRAM_H
